6ab19886d54ca052411a6e830e88edb55f607c79
Maik-Schulze <69257915+Maik-Schulze@users.noreply.github.com>
2020-08-05 20:41:48 +0200

    Update turtleBootstrap.lua

--- turtleBootstrap.lua
+++ turtleBootstrap.lua
@@ -1,1 +1,30 @@
+--[[
+turtleBootstrap.lua
+Date: 05.08.2020
+User: Maik-Schulze
 
+This is effectively a copy of 'bootstrap.lua'
+that downloads more programs for the turtle
+in the Minecraft Mod ComputerCraft.
+
+Usage:
+  > github get Maik-Schulze/CCScripts/master/turtleBootstrap.lua turtleBootstrap
+  > turtleBootstrap
+--]]
+
+http_runs = {
+	["pastebin"] = {
+	
+	},
+	["github"] = {
+		--strip mining
+		["Maik-Schulze/CCScripts/master/strip.lua"] = strip,
+	},
+}
+
+for service, list in pairs(http_runs) do
+	for id, program in pairs(list) do
+		print("Downloading "..program.." from "..service)
+		shell.run(service, "get", id, program)
+	end
+end
