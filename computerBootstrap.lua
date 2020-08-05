1b938689804792c7fda5e9d9408fbbe40ea8a370
Maik-Schulze <69257915+Maik-Schulze@users.noreply.github.com>
2020-08-05 20:52:56 +0200

    Update computerBootstrap.lua

--- computerBootstrap.lua
+++ computerBootstrap.lua
@@ -1,1 +1,27 @@
+--[[
+computerBootstrap.lua
+Date: 05.08.2020
+User: Maik-Schulze
 
+This is effectively a copy of 'bootstrap.lua'
+that downloads more programs for the computer
+in the Minecraft Mod ComputerCraft.
+
+Usage:
+  > github get Maik-Schulze/CCScripts/master/computerBootstrap.lua computerBootstrap
+  > computerBootstrap
+--]]
+
+http_runs = {
+	["github"] = {
+		--example stripmine
+		--["Maik-Schulze/CCScripts/master/strip.lua"] = strip,
+	},
+}
+
+for service, list in pairs(http_runs) do
+	for id, program in pairs(list) do
+		print("Downloading "..program.." from "..service)
+		shell.run(service, "get", id, program)
+	end
+end
