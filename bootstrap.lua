10fbdb5c9720827e1044f79d4db1a148f01f690a
Maik-Schulze <69257915+Maik-Schulze@users.noreply.github.com>
2020-08-05 20:53:25 +0200

    Update bootstrap.lua

--- bootstrap.lua
+++ bootstrap.lua
@@ -1,9 +1,79 @@
 --[[
 bootstrap.lua
+Date: 05.08.2020
+User: Maik-Schulze
 
-Program to get multiple programs at once.
+Program to download multiple programs in the Minecraft Mod ComputerCraft at once.
 
 Usage:
-	> pastebin get XXXXXXXX bootstrap
+	> pastebin get prsnEEAi bootstrap
 	> bootstrap <DEVICE-TYPE>
 --]]
+
+--get the parameters as a table and its lenght.
+local argt = { ... }
+local argc = #argt
+
+--if the parameter table has more than 1 entry give out an error message and quit.
+if (argc ~= 1) then
+	print("Syntax Error.")
+	print("For tips on correct usage type 'bootstrap help'.")
+	return
+end
+
+--extract the first parameter.
+local first = argt[1]
+
+--if the first parameter is 'help' print out some info on how to use this.
+if (first == "help") then
+	print("bootstrap <DEVICE-TYPE>")
+	print("Only <DEVICE-TYPE> and 'help' are valid parameters")
+	print("<DEVICE-TYPE> \t has to be either 'turtle' or 'computer'.")
+	return
+end
+
+--if the first parameter isn't 'turtle' or 'computer' give out an error message and quit.
+if ((first ~= "turtle") and (first ~= "computer")) then
+	print("Syntax Error.")
+	print("For tips on correct usage type 'bootstrap help'.")
+	return
+end
+
+--if the first parameter is 'turtle' download the program belonging to a turtle.
+if (first == "turtle") then
+	http_runs = {
+		["pastebin"] = {
+			--github loader
+			["zvx63NNF"] = "github",
+		},
+		["github"] = {
+			["Maik-Schulze/CCScripts/master/turtleBootstrap.lua"] = "turtleBootstrap",
+		},
+	}
+end
+
+--if the first parameter is 'computer' download the program belonging to a computer.
+if (first == "computer") then
+	http_runs = {
+		["pastebin"] = {
+			--github loader
+			["zvx63NNF"] = "github",
+		},
+		["github"] = {
+			["Maik-Schulze/CCScripts/master/computerBootstrap.lua"] = "computerBootstrap",
+		},
+	}
+end
+
+for service, list in pairs(http_runs) do
+	for id, program in pairs(list) do
+		print("Downloading "..program.." from "..service)
+		shell.run(service, "get", id, program)
+	end
+end
+
+if (first == "turtle") then
+	shell.run("turtleBootstrap")
+else
+	shell.run("computerBootstrap")
+end
