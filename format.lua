6ead4d0bdb671f165d80b56abd4128882c1f928d
Maik-Schulze <69257915+Maik-Schulze@users.noreply.github.com>
2020-08-07 16:05:15 +0200

    Create format.lua

---
+++ format.lua
@@ -0,0 +1,16 @@
+--[[
+format.lua
+Date: 07.08.2020
+User: Maik-Schulze
+ 
+Program to be able to format a computer or turtle in the Minecraft Mod ComputerCraft.
+
+Usage:
+	> format
+--]]
+
+for _, f in pairs(fs.list("/")) do
+	if not fs.isReadOnly(f) then
+		fs.delete(f)
+	end
+end
